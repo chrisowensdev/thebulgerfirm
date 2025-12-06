c358f5dead04c87746e41803ed5d6a987e2e74f4
Christopher Owens <cowensdev@gmail.com>
2025-12-06 14:30:42 -0500

    Apply patch

---
+++ src/app/practice-areas/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const metadata = {
+	title: "Practice Areas - The Bulger Firm",
+	description: "What we practice.",
+	icons: {
+		icon: "/images/logo.png", // Standard favicon
+		shortcut: "/favicon.ico", // <link rel="shortcut icon">
+		// apple: "/apple-touch-icon.png", // iOS icon
+	},
+};
+
+const PracticeAreasPage = () => {
+	return <div>PracticeAreasPage</div>;
+};
+
+export default PracticeAreasPage;
